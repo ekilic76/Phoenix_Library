460d2f70f553d1db91fd6b844e6ce44321c60f23
ekilic76 <ekilic76@yahoo.com>
2023-03-02 10:11:49 -0500

    Initial commit

---
+++ src/test/java/com/phoenix_library/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package com.phoenix_library.pages;
+
+
+
+import com.phoenix_library.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//input[@type='email']")
+    public WebElement userName;
+
+
+
+    @FindBy(xpath = "//input[@type='password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//button[.='Sign in']")
+
+    public WebElement submit;
+
+    public void login(String userNameStr, String passwordStr) {
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+        // verification that we logged
+    }
+
+}
---
+++ src/test/java/com/phoenix_library/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.phoenix_library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/phoenix_library/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
