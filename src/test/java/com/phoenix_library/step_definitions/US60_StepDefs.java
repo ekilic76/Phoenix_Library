bcfa63ac2e40b8f8fcc41b501cac996079cf9bca
erkineser <eseremre@yahoo.com>
2023-03-07 22:54:06 -0500

    I finished my code in stepDefs

---
+++ src/test/java/com/phoenix_library/step_definitions/US60_StepDefs.java
@@ -0,0 +1,50 @@
+package com.phoenix_library.step_definitions;
+
+import com.phoenix_library.pages.BasePage;
+import com.phoenix_library.pages.UsersPage;
+import com.phoenix_library.utilities.BrowserUtils;
+import com.phoenix_library.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.commons.collections.list.UnmodifiableList;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.RandomAccess;
+
+public class US60_StepDefs {
+
+    UsersPage usersPage = new UsersPage();
+
+    @When("the user clicks on {string} link")
+    public void the_user_clicks_on_link(String string) {
+    usersPage.usersLink.click();
+    BrowserUtils.sleep(2);
+    }
+
+    @Then("show records default value should be {int}")
+    public void show_records_default_value_should_be(Integer int1) {
+        Select dropDown = new Select(usersPage.recordsDropDown);
+
+        String actualOption = dropDown.getFirstSelectedOption().getText();
+        String expectedOption = "10";
+        Assert.assertEquals(actualOption, expectedOption);
+    }
+
+    @Then("show record should have following options:")
+    public void show_record_should_have_following_options(List<String> expectedOptions) {
+        List<String> actualOptions = BrowserUtils.dropdownOptions_as_STRING(usersPage.recordsDropDown);
+
+        actualOptions = Collections.unmodifiableList(actualOptions);
+
+        Assert.assertEquals(actualOptions, expectedOptions);
+
+    }
+
+}
+
