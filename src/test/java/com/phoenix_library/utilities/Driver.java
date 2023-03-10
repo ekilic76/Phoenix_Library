64394ec712d6823b374c20b36643f8bd04477007
erkine <114306508+erkine@users.noreply.github.com>
2023-03-10 01:08:08 -0500

    Update Driver.java

--- src/test/java/com/phoenix_library/utilities/Driver.java
+++ src/test/java/com/phoenix_library/utilities/Driver.java
@@ -1,6 +1,5 @@
 package com.phoenix_library.utilities;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -41,23 +40,23 @@ public class Driver {
              */
             switch (browserType){
                 case "chrome":
-
-                  //  WebDriverManager.chromedriver().setup();
-                    ChromeOptions options=new ChromeOptions();
-
                     //WebDriverManager.chromedriver().setup();
-                    ChromeOptions options = new ChromeOptions();
 
+
+                   
+
+                    ChromeOptions options = new ChromeOptions();
                     options.addArguments("--remote-allow-origins=*");
                     driverPool.set(new ChromeDriver(options));
+
                     driverPool.get().manage().window().maximize();
-                //    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
                 case "firefox":
                     //WebDriverManager.firefoxdriver().setup();
                     driverPool.set(new FirefoxDriver());
                     driverPool.get().manage().window().maximize();
-                //    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
             }
 
