4427af48ea785faaf3d36b4b68833d5c81b6111c
erkine <114306508+erkine@users.noreply.github.com>
2023-03-10 00:38:20 -0500

    Update CukesRunner.java

--- src/test/java/com/phoenix_library/runners/CukesRunner.java
+++ src/test/java/com/phoenix_library/runners/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "json:target/cucumber.json",
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
@@ -16,8 +17,10 @@ import org.junit.runner.RunWith;
         dryRun = false,
 
 
+  
+        tags = "",
+
+
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
-
-
