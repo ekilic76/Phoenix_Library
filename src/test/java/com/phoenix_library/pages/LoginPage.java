6ae7d5c7bc738ed549618a6a6c7503e0e4946ce1
Biljana523 <115359314+Biljana523@users.noreply.github.com>
2023-03-10 00:00:41 -0500

    Update LoginPage.java

--- src/test/java/com/phoenix_library/pages/LoginPage.java
+++ src/test/java/com/phoenix_library/pages/LoginPage.java
@@ -7,13 +7,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage extends BasePage{
+public class LoginPage{
 
-  /*  public LoginPage(){
+    public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-   */
+   
 
     @FindBy(xpath = "//input[@type='email']")
     public WebElement userName;
